997012ae897900b510f3340dec3a3f6e2e0a596f
marial711 <50689685+marial711@users.noreply.github.com>
2021-03-30 09:16:11 -0400

    added Pay Now button!

---
+++ form.php
@@ -0,0 +1,14 @@
+<?php
+$fname =  $_POST["fname"];
+$lname = $_POST["lname"];
+$email = $_POST["email"];
+$number = $_POST["phonenumber"];
+$zipcode = $_POST["zipcode"];
+$moreinfo = $_POST["moreinfo"];
+
+echo $fname.$lname;
+echo $email;
+echo $number;
+echo $zipcode;
+echo $moreinfo;
+ ?>
---
+++ js/app.js
@@ -0,0 +1,8 @@
+window.onload = () => {
+  let paybutton = document.querySelector(`button`);
+
+  paybutton.addEventListener(`click`, function(e){
+    let message = "Heres the message";
+    alert(message);
+  })
+}
