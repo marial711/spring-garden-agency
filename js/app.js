2db52f3a381b7e5de5f950f6c171567f475925bb
marial711 <50689685+marial711@users.noreply.github.com>
2021-04-04 15:28:05 -0400

    Functioning Pop up!
    Work on close button aspect

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
 window.onload = () => {
-  
+  let paybutton = document.querySelector(`button`);
+  let close = document.getElementById(`close-btn`);
+
+  paybutton.addEventListener(`click`, function(e){
+    document.getElementById(`popup-1`).classList.toggle(`active`);
+  });
+
+  close.addEventListener(`click`, function(e){
+    document.getElementById(`popup-1`).classList.toggle(`active`);
+  });
   }
